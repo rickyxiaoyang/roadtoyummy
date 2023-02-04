04b72305aa56a080e329de6db070fef0ae64ff20
Ricky <iarexd@gmail.com>
2023-02-04 11:54:07 -0500

    front: capitalize title

--- src/components/recipes/RecipeList.tsx
+++ src/components/recipes/RecipeList.tsx
@@ -1,80 +1,80 @@
 import React from "React";
 import { graphql, navigate, useStaticQuery } from "gatsby";
 import {
-    recipeList,
-    recipeItem,
-    recipeItemImageContainer,
-    recipeItemTextContainer,
+	recipeList,
+	recipeItem,
+	recipeItemImageContainer,
+	recipeItemTextContainer,
 } from "./RecipeList.module.scss";
 
 type Recipe = {
-    slug: string;
-    title: string;
-    date: Date;
-    short_description: string;
-    featured_image: string;
+	slug: string;
+	title: string;
+	date: Date;
+	short_description: string;
+	featured_image: string;
 };
 
 const pageQuery = graphql`
-    query allRecipes {
-        allMarkdownRemark {
-            nodes {
-                frontmatter {
-                    slug
-                    title
-                    date(formatString: "MMMM, DD, YYYY")
-                    short_description
-                    featured_image
-                }
-            }
-        }
-    }
+	query allRecipes {
+		allMarkdownRemark {
+			nodes {
+				frontmatter {
+					slug
+					title
+					date(formatString: "MMMM, DD, YYYY")
+					short_description
+					featured_image
+				}
+			}
+		}
+	}
 `;
 
 export const RecipeList = () => {
-    const data = useStaticQuery(pageQuery);
-    const { nodes }: { nodes: any[] } = data?.allMarkdownRemark ?? [];
-    return (
-        <div className={recipeList}>
-            {nodes.map((recipe) => (
-                <div key={recipe.slug}>
-                    <RecipeItem recipe={recipe.frontmatter} />
-                    <hr
-                        style={{
-                            maxWidth: "700px",
-                            width: "90%",
-                            opacity: "0.2",
-                        }}
-                    />
-                </div>
-            ))}
-        </div>
-    );
+	const data = useStaticQuery(pageQuery);
+	const { nodes }: { nodes: any[] } = data?.allMarkdownRemark ?? [];
+	return (
+		<div className={recipeList}>
+			{nodes.map((recipe) => (
+				<div key={recipe.slug}>
+					<RecipeItem recipe={recipe.frontmatter} />
+					<hr
+						style={{
+							maxWidth: "700px",
+							width: "90%",
+							opacity: "0.2",
+						}}
+					/>
+				</div>
+			))}
+		</div>
+	);
 };
 
 const RecipeItem = ({ recipe }: { recipe: Recipe }) => {
-    return (
-        <div className={recipeItem}>
-            <div
-                className={recipeItemImageContainer}
-                style={{
-                    background: `url(${recipe.featured_image})`,
-                    backgroundRepeat: "no-repeat",
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                }}
-            ></div>
-            <div className={recipeItemTextContainer}>
-                <h2>{recipe.title}</h2>
-                <span>{recipe.short_description}</span>
-                <br />
-                <button
-                    className="btn filled"
-                    onClick={() => navigate(`recipes/${recipe.slug}`)}
-                >
-                    Learn more...
-                </button>
-            </div>
-        </div>
-    );
+	return (
+		<div className={recipeItem}>
+			<div
+				className={recipeItemImageContainer}
+				style={{
+					background: `url(${recipe.featured_image})`,
+					backgroundRepeat: "no-repeat",
+					backgroundSize: "cover",
+					backgroundPosition: "center",
+				}}
+			></div>
+			<div className={recipeItemTextContainer}>
+				<h2>{recipe.title.toUpperCase()}</h2>
+				<span>{recipe.short_description}</span>
+				<br />
+				<button
+					className="btn filled"
+					onClick={() => navigate(`recipes/${recipe.slug}`)}
+				>
+					Learn more...
+				</button>
+			</div>
+		</div>
+	);
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,23 +4,23 @@ import { RecipeList } from "../components/recipes/RecipeList";
 import Layout from "../components/shared/Layout/Layout";
 
 const IndexPage: React.FC<PageProps> = () => {
-    return (
-        <main>
-            <Layout>
-                <RecipeList />
-            </Layout>
-        </main>
-    );
+	return (
+		<main>
+			<Layout>
+				<RecipeList />
+			</Layout>
+		</main>
+	);
 };
 
 export default IndexPage;
 
 export const Head: HeadFC = () => (
-    <>
-        <title>Home Page</title>
-        <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-        ></meta>
-    </>
+	<>
+		<title>Road to Yummy</title>
+		<meta
+			name="viewport"
+			content="width=device-width, initial-scale=1.0"
+		></meta>
+	</>
 );
