e9eb9d024004eff6180f278ba348b9faab74f508
Ricky <iarexd@gmail.com>
2023-02-04 11:50:55 -0500

    feat: add images to recipe directions

--- src/components/recipes/Direction/Direction.tsx
+++ src/components/recipes/Direction/Direction.tsx
@@ -1,34 +1,48 @@
 import React, { useEffect } from "react";
+import "./Direction.scss";
 
 type Direction = {
-  text: string;
-  image?: string;
+	text: string;
+	image?: string;
 };
 
 export type DirectionSection = {
-  name: string;
-  steps: Direction[];
+	name: string;
+	steps: Direction[];
 };
 
 export function DirectionSection({
-  section,
-  hideSectionTitle = false,
+	section,
+	hideSectionTitle = false,
 }: {
-  section: DirectionSection;
-  hideSectionTitle: boolean;
+	section: DirectionSection;
+	hideSectionTitle: boolean;
 }) {
-  return (
-    <div className="direction-section">
-      {hideSectionTitle ? <></> : <h4>{section.name}</h4>}
-      <ol>
-        {section.steps.map((step, index) => (
-          <DirectionItem key={index} direction={step} />
-        ))}
-      </ol>
-    </div>
-  );
+	return (
+		<div className="direction-section">
+			{hideSectionTitle ? (
+				<></>
+			) : (
+				<div className="direction-section-title">{section.name}</div>
+			)}
+			<ol className="direction-list">
+				{section.steps.map((step, index) => (
+					<DirectionItem key={index} direction={step} />
+				))}
+			</ol>
+		</div>
+	);
 }
 
 function DirectionItem({ direction }: { direction: Direction }) {
-  return <li>{direction.text}</li>;
+	return (
+		<li className="direction-item">
+			<div className="direction-item-text">{direction.text}</div>
+			{direction.image && (
+				<div className="direction-item-image">
+					<img src={direction.image}></img>
+				</div>
+			)}
+		</li>
+	);
 }
