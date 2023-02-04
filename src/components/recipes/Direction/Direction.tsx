2fe951e38e210699dd2293d093d354de0af3ce30
Ricky <iarexd@gmail.com>
2023-02-04 12:07:52 -0500

    show/hide images

--- src/components/recipes/Direction/Direction.tsx
+++ src/components/recipes/Direction/Direction.tsx
@@ -14,9 +14,11 @@ export type DirectionSection = {
 export function DirectionSection({
 	section,
 	hideSectionTitle = false,
+	showImages,
 }: {
 	section: DirectionSection;
 	hideSectionTitle: boolean;
+	showImages: boolean;
 }) {
 	return (
 		<div className="direction-section">
@@ -27,18 +29,24 @@ export function DirectionSection({
 			)}
 			<ol className="direction-list">
 				{section.steps.map((step, index) => (
-					<DirectionItem key={index} direction={step} />
+					<DirectionItem key={index} direction={step} showImage={showImages} />
 				))}
 			</ol>
 		</div>
 	);
 }
 
-function DirectionItem({ direction }: { direction: Direction }) {
+function DirectionItem({
+	direction,
+	showImage,
+}: {
+	direction: Direction;
+	showImage: boolean;
+}) {
 	return (
 		<li className="direction-item">
 			<div className="direction-item-text">{direction.text}</div>
-			{direction.image && (
+			{direction.image && showImage && (
 				<div className="direction-item-image">
 					<img src={direction.image}></img>
 				</div>
--- src/pages/recipes/{markdownRemark.frontmatter__slug}.tsx
+++ src/pages/recipes/{markdownRemark.frontmatter__slug}.tsx
@@ -4,79 +4,108 @@ import Layout from "../../components/shared/Layout/Layout";
 import "../../styles/styles.scss";
 import "./recipes.scss";
 import {
-  Ingredient,
-  IngredientItem,
+	Ingredient,
+	IngredientItem,
 } from "../../components/recipes/Ingredient/Ingredient";
 import { DirectionSection } from "../../components/recipes/Direction/Direction";
 
 export default function RecipeTemplate({ data }: { data: any }) {
-  const { markdownRemark } = data;
-  const { frontmatter } = markdownRemark;
-  const { recipe } = frontmatter;
-  const { direction_sections } = recipe;
+	const { markdownRemark } = data;
+	const { frontmatter } = markdownRemark;
+	const { recipe } = frontmatter;
+	const { direction_sections } = recipe;
 
-  return (
-    <Layout>
-      <RecipeLayout>
-        <div>
-          <h1>{frontmatter.title}</h1>
-          <p>{frontmatter.date}</p>
-          <img
-            className="recipe-featured-image"
-            src={frontmatter.featured_image}
-          />
+	const [showImages, setShowImages] = React.useState<boolean>(true);
 
-          <div>
-            <h3>Ingredients</h3>
-            {recipe.ingredients.map((ingredient: Ingredient) => (
-              <IngredientItem key={ingredient.name} ingredient={ingredient} />
-            ))}
-          </div>
-          <div>
-            <h3>Directions</h3>
-            {direction_sections.map((section: DirectionSection) => (
-              <DirectionSection
-                key={section.name}
-                section={section}
-                hideSectionTitle={direction_sections.length == 1}
-              />
-            ))}
-          </div>
-        </div>
-      </RecipeLayout>
-    </Layout>
-  );
+	const ingredients = (
+		<div>
+			<h3>Ingredients</h3>
+			{recipe.ingredients.map((ingredient: Ingredient) => (
+				<IngredientItem key={ingredient.name} ingredient={ingredient} />
+			))}
+		</div>
+	);
+
+	const directions = (
+		<div>
+			<div className="direction-title">
+				{directionSectionsHaveImages(direction_sections) && (
+					<button
+						style={{ float: "right" }}
+						type="button"
+						className="btn outline"
+						onClick={() => setShowImages((bool) => !bool)}
+					>
+						{showImages ? `Hide` : `Show`} images
+					</button>
+				)}
+				<h3>Directions</h3>
+			</div>
+
+			{direction_sections.map((section: DirectionSection) => (
+				<DirectionSection
+					key={section.name}
+					section={section}
+					hideSectionTitle={direction_sections.length == 1}
+					showImages={showImages}
+				/>
+			))}
+		</div>
+	);
+
+	return (
+		<Layout>
+			<RecipeLayout>
+				<div>
+					<h1>{frontmatter.title}</h1>
+					<p>{frontmatter.date}</p>
+					<img
+						className="recipe-featured-image"
+						src={frontmatter.featured_image}
+					/>
+					{ingredients}
+					{directions}
+				</div>
+			</RecipeLayout>
+		</Layout>
+	);
 }
 
 function RecipeLayout({ children }: { children: JSX.Element }) {
-  return <div className="recipe-container">{children}</div>;
+	return <div className="recipe-container">{children}</div>;
 }
 
 export const pageQuery = graphql`
-  query ($id: String!) {
-    markdownRemark(id: { eq: $id }) {
-      html
-      frontmatter {
-        date(formatString: "MMMM DD, YYYY")
-        slug
-        title
-        categories
-        featured_image
-        recipe {
-          ingredients {
-            name
-            amount
-            unit
-          }
-          direction_sections {
-            name
-            steps {
-              text
-              image
-            }
-          }
-        }
-      }
-    }
-  }
+	query ($id: String!) {
+		markdownRemark(id: { eq: $id }) {
+			html
+			frontmatter {
+				date(formatString: "MMMM DD, YYYY")
+				slug
+				title
+				categories
+				featured_image
+				recipe {
+					ingredients {
+						name
+						amount
+						unit
+					}
+					direction_sections {
+						name
+						steps {
+							text
+							image
+						}
+					}
+				}
+			}
+		}
+	}
 `;
+
+function directionSectionsHaveImages(sections: DirectionSection[]) {
+	return sections
+		.map((s) => s.steps.filter((step) => !!step.image).length > 0)
+		.includes(true);
+}
