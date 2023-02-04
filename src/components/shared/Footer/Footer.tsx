f37daa0df3b953fbf122a1b6be9e00cc6fbea3ad
Ricky <iarexd@gmail.com>
2023-02-04 17:03:13 -0500

    feat: add footer

---
+++ src/components/shared/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import * as styles from "./Footer.module.scss";
+
+export function Footer() {
+	return (
+		<div className={styles.footer}>
+			<div>ROAD TO YUMMY</div>
+			<div>by Amy & Ricky</div>
+		</div>
+	);
+}
--- src/components/shared/Layout/Layout.tsx
+++ src/components/shared/Layout/Layout.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import Header from "../Header/Header";
 import "../../../styles/styles.scss";
+import { Footer } from "../Footer/Footer";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div>
-      <Header />
-      {children}
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			{children}
+			<Footer />
+		</div>
+	);
 }
